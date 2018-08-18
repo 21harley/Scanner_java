9af65f68080ca9ddc6553fb78f6f6a4aee03c532
john llanes <jhonharleyllanesescobar@hotmail.com>
2018-08-18 14:48:31 -0400

    primero

---
+++ src/hola/Hola.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola;
+import java.io.*;
+import java.util.Scanner;
+/**
+ *
+ * @author CASA
+ */
+public class Hola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double sueldo;
+        int edad;
+        float notas,notasAcum=0;
+        Scanner in =new Scanner(System.in);
+        System.out.println("Ingrese los proximos datos");
+        
+        
+    }
+    
+}
